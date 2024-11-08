b680977a87fffa4589ffc71e3cbd7c4f866b3683
deni-cyber <deniskamau928@gmail.com>
2024-11-08 10:46:14 +0300

    mino changes on templates (product listing)

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# In carts/views.py
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
 from products.models import Product
